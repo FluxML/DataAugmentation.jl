597b3d95ec8503580d6dfefae89097fe76e5e31d
Lorenz Ohly <lorenz.ohly@gmail.com>
2020-11-10 15:09:12 +0100

    self-built

---
+++ dev/versions.js
@@ -0,0 +1,6 @@
+var PUBLISH_ROOT = '/DataAugmentation.jl/dev';
+var PUBLISH_VERSION = "dev";
+var PUBLISH_VERSIONS = [
+    ["dev","/DataAugmentation.jl/dev/index.html"],
+    ["0.1.0","/DataAugmentation.jl/0.1.0/index.html"],
+];
