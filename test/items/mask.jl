7e101d84840f1af118221acff76af920095368ae
Lorenz Ohly <lorenz.ohly@gmail.com>
2020-11-28 21:46:45 +0100

    fix tests pt. 3

--- test/items/mask.jl
+++ test/items/mask.jl
@@ -6,6 +6,7 @@ include("../imports.jl")
     tfm = Project(Translation(20, 10))
 
     @test_nowarn apply(tfm, mask)
+    tmask = apply(tfm, mask)
     @test boundsranges(tmask.bounds) == (21:52, 11:42)
 
     @test_nowarn apply!(tmask, tfm, mask)
