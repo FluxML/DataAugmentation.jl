b9df93511fc96505b021b35af1c718d726f4f647
lorenzoh <lorenz.ohly@gmail.com>
2021-02-03 14:02:19 +0100

    fix docs pt.4

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
-using Artifacts
 using Publish
 using DataAugmentation
+using Pkg.Artifacts
 
 
-artifactsfile = download("https://raw.githubusercontent.com/darsnack/flux-theme/main/Artifacts.toml", joinpath(@__DIR__, "Artifacts.toml"))
 Publish.Themes.default() = artifact"flux-theme"
-rm(joinpath(@__DIR__, "Artifacts.toml"))
 
 p = Publish.Project(DataAugmentation)
+
 rm("dev", recursive = true, force = true)
 rm(p.env["version"], recursive = true, force = true)
+
 deploy(DataAugmentation; root = "/DataAugmentation.jl", force = true, label = "dev")
