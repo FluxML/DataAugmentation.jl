72331dcb1f92f213efcb760fdb21158775790765
lorenzoh <lorenz.ohly@gmail.com>
2021-02-03 12:16:55 +0100

    clean up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 include("./imports.jl")
 
 
-include("base.jl")
-include("buffered.jl")
-include("projective/base.jl")
-include("projective/bounds.jl")
-include("projective/compose.jl")
-include("projective/crop.jl")
-include("projective/affine.jl")
-include("items/image.jl")
-include("items/keypoints.jl")
-include("items/mask.jl")
-include("preprocessing.jl")
+@testset ExtendedTestSet "DataAugmentation.jl" begin
+    include("base.jl")
+    include("buffered.jl")
+    include("projective/base.jl")
+    include("projective/bounds.jl")
+    include("projective/compose.jl")
+    include("projective/crop.jl")
+    include("projective/affine.jl")
+    include("items/image.jl")
+    include("items/keypoints.jl")
+    include("items/mask.jl")
+    include("preprocessing.jl")
+end
