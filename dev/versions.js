d96c8ce99f2c8a2a246316e3ee7eb8e2e874ec56
lorenzoh <lorenz.ohly@gmail.com>
2021-04-14 11:19:35 +0200

    update docs

--- dev/versions.js
+++
@@ -1,6 +0,0 @@
-var PUBLISH_ROOT = '/DataAugmentation.jl/dev';
-var PUBLISH_VERSION = "dev";
-var PUBLISH_VERSIONS = [
-    ["dev","/DataAugmentation.jl/dev/index.html"],
-    ["0.1.0","/DataAugmentation.jl/0.1.0/index.html"],
-];
