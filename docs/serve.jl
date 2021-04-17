c11e9c33b5c8650db342eb3fa233e8f52f2605ab
lorenzoh <lorenz.ohly@gmail.com>
2021-04-17 15:20:35 +0200

    improve live docs script

--- docs/serve.jl
+++ docs/serve.jl
@@ -2,8 +2,5 @@ using Pkg
 using Pollen
 using DataAugmentation
 
-p = Pollen.documentationproject(DataAugmentation, executecode=true)
-push!(p.rewriters, Pollen.PackageWatcher([DataAugmentation]))
-server = Server(p)
-mode = ServeFilesLazy()
-runserver(server, mode)
+p = Pollen.documentationproject(DataAugmentation)
+Pollen.serve(p)
