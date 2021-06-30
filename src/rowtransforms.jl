cd42f1d4c4e34e84559a0dd620544792309f4aa1
Manikya <manikyabard@gmail.com>
2021-07-01 00:05:19 +0530

    updated Categorify to use SortedSet

--- src/rowtransforms.jl
+++ src/rowtransforms.jl
@@ -12,16 +12,18 @@ struct Categorify{T, S}
     dict::T
     cols::S
     function Categorify{T, S}(dict::T, cols::S) where {T, S}
+        newdict = Dict()
         for (col, vals) in dict
-            dict[col] = append!([], [missing], collect(skipmissing(Set(vals))))
+            newdict[col] = DataStructures.SortedSet{Union{Missing, eltype(vals)}}(Base.Order.ReverseOrdering(), vals)
+            push!(newdict[col], missing)
         end
-        new{T, S}(dict, cols)
+        new{typeof(newdict), S}(newdict, cols)
     end
 end
 
 Categorify(dict::T, cols::S) where {T, S} = Categorify{T, S}(dict, cols)
 
-function apply(tfm::NormalizeRow, item; randstate=nothing)
+function apply(tfm::NormalizeRow, item::TabularItem; randstate=nothing)
     x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
         if col in tfm.cols
             colmean, colstd = tfm.dict[col]
@@ -29,22 +31,26 @@ function apply(tfm::NormalizeRow, item; randstate=nothing)
         end
         (col, val)
     end)
+    TabularItem(x, item.columns)
 end
 
-function apply(tfm::FillMissing, item; randstate=nothing)
+function apply(tfm::FillMissing, item::TabularItem; randstate=nothing)
     x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
         if col in tfm.cols && ismissing(val)
             val = tfm.dict[col]
         end
         (col, val)
     end)
+    TabularItem(x, item.columns)
 end
 
-function apply(tfm::Categorify, item; randstate=nothing)
+function apply(tfm::Categorify, item::TabularItem; randstate=nothing)
     x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
         if col in tfm.cols
-            val = ismissing(val) ? 1 : findfirst(val .== tfm.dict[col]) + 1
+            val = ismissing(val) ? 1 : findfirst(val .== skipmissing(tfm.dict[col])) + 1
+            # val = findkey(tfm.dict[col], val).address
         end
         (col, val)
     end)
+    TabularItem(x, item.columns)
 end
