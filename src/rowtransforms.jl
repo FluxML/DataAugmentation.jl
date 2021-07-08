128cf97574fa9990d08c14e9eb88f286c729f297
Manikya <manikyabard@gmail.com>
2021-07-08 16:20:32 +0530

    added docstrings for tabular transforms

--- src/rowtransforms.jl
+++ src/rowtransforms.jl
@@ -1,13 +1,103 @@
+"""
+    NormalizeRow(dict, cols)
+
+Normalizes the values of a row present in `TabularItem` for the columns 
+specified in `cols` using `dict`, which contains the column names as 
+dictionary keys and the mean and standard deviation tuple present as 
+dictionary values.
+
+## Example
+
+```julia
+using DataAugmentation
+
+cols = [:col1, :col2, :col3]
+row = (; zip(cols, [1, 2, 3])...)
+item = TabularItem(row, cols)
+normdict = Dict(:col1 => (1, 1), :col2 => (2, 2))
+
+tfm = NormalizeRow(normdict, [:col1, :col2])
+apply(tfm, item)
+```
+"""
 struct NormalizeRow{T, S} <: Transform
     dict::T
     cols::S
 end
 
+function apply(tfm::NormalizeRow, item::TabularItem; randstate=nothing)
+    x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
+        if col in tfm.cols
+            colmean, colstd = tfm.dict[col]
+            val = (val - colmean)/colstd
+        end
+        (col, val)
+    end)
+    TabularItem(x, item.columns)
+end
+
+"""
+    FillMissing(dict, cols)
+
+Fills the missing values of a row present in `TabularItem` for the columns 
+specified in `cols` using `dict`, which contains the column names as 
+dictionary keys and the value to fill the column with present as 
+dictionary values.
+
+## Example
+
+```julia
+using DataAugmentation
+
+cols = [:col1, :col2, :col3]
+row = (; zip(cols, [1, 2, 3])...)
+item = TabularItem(row, cols)
+fmdict = Dict(:col1 => 100, :col2 => 100)
+
+tfm = FillMissing(fmdict, [:col1, :col2])
+apply(tfm, item)
+```
+"""
 struct FillMissing{T, S} <: Transform
     dict::T
     cols::S
 end
 
+function apply(tfm::FillMissing, item::TabularItem; randstate=nothing)
+    x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
+        if col in tfm.cols && ismissing(val)
+            val = tfm.dict[col]
+        end
+        (col, val)
+    end)
+    TabularItem(x, item.columns)
+end
+
+"""
+    Categorify(dict, cols)
+
+Label encodes the values of a row present in `TabularItem` for the 
+columns specified in `cols` using `dict`, which contains the column 
+names as dictionary keys and the unique values of column present 
+as dictionary values.
+
+if there are any `missing` values in the values to be transformed, 
+they are replaced by 1.
+
+## Example
+
+```julia
+using DataAugmentation
+
+cols = [:col1, :col2, :col3]
+row = (; zip(cols, ["cat", 2, 3])...)
+item = TabularItem(row, cols)
+catdict = Dict(:col1 => ["dog", "cat"])
+
+tfm = Categorify(catdict, [:col1])
+apply(tfm, item)
+```
+"""
 struct Categorify{T, S} <: Transform
     dict::T
     cols::S
@@ -23,27 +113,6 @@ struct Categorify{T, S} <: Transform
 end
 
 Categorify(dict::T, cols::S) where {T, S} = Categorify{T, S}(dict, cols)
-
-function apply(tfm::NormalizeRow, item::TabularItem; randstate=nothing)
-    x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
-        if col in tfm.cols
-            colmean, colstd = tfm.dict[col]
-            val = (val - colmean)/colstd
-        end
-        (col, val)
-    end)
-    TabularItem(x, item.columns)
-end
-
-function apply(tfm::FillMissing, item::TabularItem; randstate=nothing)
-    x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
-        if col in tfm.cols && ismissing(val)
-            val = tfm.dict[col]
-        end
-        (col, val)
-    end)
-    TabularItem(x, item.columns)
-end
 
 function apply(tfm::Categorify, item::TabularItem; randstate=nothing)
     x = NamedTuple(Iterators.map(item.columns, item.data) do col, val
