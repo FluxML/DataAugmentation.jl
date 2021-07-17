1a227916be083631595b903ade9973c7e8ad9541
Manikya Bardhan <manikyabard@gmail.com>
2021-07-18 02:00:53 +0530

    updated tests
    Co-authored-by: Brian Chen <ToucheSir@users.noreply.github.com>

--- test/rowtransforms.jl
+++ test/rowtransforms.jl
@@ -2,7 +2,7 @@ include("imports.jl")
 
 @testset ExtendedTestSet "`NormalizeRow`" begin
     cols = [:col1, :col2, :col3]
-    item = TabularItem((; zip(cols, [1, "a", 10])...), cols)
+    item = TabularItem(NamedTuple(zip(cols, [1, "a", 10])), cols)
     cols_to_normalize = [:col1, :col3]
     col1_mean, col1_std = 10, 100
     col3_mean, col3_std = 100, 10
@@ -22,13 +22,12 @@ end
     cols_to_fill = [:col1, :col3]
     col1_fmval = 1000.
     col3_fmval = 1000.
-    fmdict = Dict()
-    fmdict[:col1], fmdict[:col3] = col1_fmval, col3_fmval
+    fmdict = Dict(:col1 => col1_fmval, :col3 => col3_fmval)
 
     tfm1 = FillMissing(fmdict, cols_to_fill)
     @test_nowarn apply(tfm1, item)
     titem = apply(tfm1, item)
-    @test titem.data[:col1] == (ismissing(item.data[:col1]) ? col1_fmval : item.data[:col1])
+    @test titem.data[:col1] == coalesce(item.data[:col1], col1_fmval)
     @test titem.data[:col3] == (ismissing(item.data[:col3]) ? col3_fmval : item.data[:col3])
     @test ismissing(titem.data[:col2])
 
@@ -46,7 +45,7 @@ end
     
     categorydict = Dict(:col2 => ["a", "b", "c"], :col3 => ["C", "B", "A"], :col4 => [missing, 10, 20])
     tfm = Categorify(categorydict, cols_to_categorify)
-    @test !any(ismissing.(tfm.dict[:col4]))
+    @test !any(ismissing, tfm.dict[:col4])
     @test_nowarn apply(tfm, item)
     testapply(tfm, item)
     titem = apply(tfm, item)
