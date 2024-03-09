aa457aa44be0da39401d586548ec5a4c343acdbc
darsnack <daruwalla.k.public@icloud.com>
2024-03-09 09:04:26 -0500

    Add deployment CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,33 @@
 using Documenter, DataAugmentation
 
-makedocs(sitename="DataAugmentation.jl",
-  pages = [
-    "index.md",
-    "Quickstart" => "quickstart.md",
-    "Transformations" => "transformations.md",
-    "Build your transformations" =>[
-      "Item Interface" => "iteminterface.md",
-      "Transform Interface" => "tfminterface.md", 
-      "Projective Interface" => [
-        "Intro" => "projective/intro.md",
-        "Data" => "projective/data.md",
-        "Gallery" => "projective/gallery.md",
-      ],
+makedocs(;
+    modules = [DataAugmentation],
+    sitename="DataAugmentation.jl",
+    pages = [
+        "index.md",
+        "Quickstart" => "quickstart.md",
+        "Transformations" => "transformations.md",
+        "Build your transformations" =>[
+            "Item Interface" => "iteminterface.md",
+            "Transform Interface" => "tfminterface.md", 
+            "Projective Interface" => [
+                "Intro" => "projective/intro.md",
+                "Data" => "projective/data.md",
+                "Gallery" => "projective/gallery.md",
+            ],
+        ],
+        "Misc" =>[
+            "Buffering" => "buffering.md",
+            "Preprocessing" => "preprocessing.md",
+            "References" => "ref.md"
+        ],
     ],
-    "Misc" =>[
-      "Buffering" => "buffering.md",
-      "Preprocessing" => "preprocessing.md",
-      "References" => "ref.md"
-    ],
-  ],
-  # format = Documenter.HTML(prettyurls = false)
+    warnonly = [:example_block, :missing_docs, :cross_references],
+    format = Documenter.HTML(canonical = "https://fluxml.ai/DataAugmentation.jl/stable/",
+                             assets = ["assets/flux.css"],
+                             prettyurls = get(ENV, "CI", nothing) == "true")
 )
+
+deploydocs(repo = "github.com/FluxML/DataAugmentation.jl.git",
+           target = "build",
+           push_preview = true)
